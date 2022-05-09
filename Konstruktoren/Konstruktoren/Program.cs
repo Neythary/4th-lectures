0e562ffd2228dff50f0a6f712156bfa18593036d
Neythary <TheNight.Kim@googlemail.com>
2022-05-09 13:06:52 +0200

    Project Konstruktoren created

---
+++ Konstruktoren/Konstruktoren/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+DateTime day1 = new DateTime();
+
+DateTime day2 = new(2001, 09, 11);
+
+var day3 = new DateTime(2001, 09, 11);
+
+
+//var day4 = new DateTime
+//{
+//    year = 2001
+//};
+// -> nur getter-Methode, keine Setter
+
+string year = "2001";
+bool parsed = int.TryParse(year, out int yearParsed);
+
+if (parsed)
+{
+    Console.WriteLine(yearParsed);
+}
+else { Console.WriteLine("ERROR!"); }
